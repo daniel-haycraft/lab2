f2714c9b2ce7f6abb418ce7567f1d871a735c022
Daniel Haycraft <dhcopy1@gmail.com>
2022-08-25 17:17:08 -0700

    finished

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -46,14 +46,14 @@ let evensArr = []
 
 //Code Here
 
-//  for (let i = 0; nums.length > i; i++){
-//   console.log(nums[i])
-//   } 
-// if (nums.length % 2 === 0){
-//   nums.push(evensArr)
-//   console.log(evensArr)
-// } 
+//  for (let i = 0; i < nums.length; i++){
+//    if (nums[i] % 2 === 0){
+//     evensArr = evensArr.concat(nums[i]);
+//     // console.log(`${evensArr}`)
+//    }
+//  }
 
+// console.log(evensArr)
 ////////// PROBLEM 5 //////////
 
 // Do not edit the code below.
@@ -113,15 +113,15 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Do not edit the code above.
 
 // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
-// for (i = 0; listOfNumbers.length >= i; i++){
-// if(listOfNumbers[i] % 3 === 0){
-//  listOfNumbers.pop(i % 3)
-//  console.log(listOfNumbers(i % 3)
+// for (let i = 0; i < listOfNumbers.length; i++){
+//   if (listOfNumbers[i] % 3 === 0){
+//     console.log(`${listOfNumbers[i]} is divisible by 3`)
+//   }
 // }
+  
 
-// }
 //Code Here
-I AM STUCK ON PROBLEMS 4 AND 8
+
 
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
@@ -133,9 +133,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-for (let i = letters.length - 1; i >= 0; i--){
-  console.log(letters[i])
-}
+// for (let i = letters.length - 1; i >= 0; i--){
+//   console.log(letters[i])
+
 
 ////////// Advanced Problems //////////
 
@@ -157,12 +157,34 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+switch (letterGrade){
+  case 'A':
+    letterGrade = console.log("The student is doing excellently")
+    break;
+  case 'B':
+    letterGrade = console.log("The student is doing well")
+    break;
+  case "B":
+    letterGrade = console.log("The student is doing alright")
+    break;
+  case "E":
+    letterGrade = console.log("The student is not doing very well")
+    break;
+  case "F":
+    letterGrade = console.log("The student is failing")
+    break;
+  default: 
+    console.log("N/A")
+}
 
 
 
 ////////// PROBLEM 11 //////////
 /* The famous FizzBuzz, Devmountain style!
   Create a for loop that iterates from 1 to 100. In this for loop, using some conditional logic, if the number your for loop is currently on is divisible by 3, console.log 'Dev'. If the number is divisible by 5, console.log 'mountain'. If the number is divisible by 5 & 3, console.log 'Devmountain'. If the number is not divisible by 5 or 3, console.log the number itself. Hint: Look up the modulo operator.
+
+
+
 
   Your output should look like:
   1
@@ -185,3 +207,16 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+for (let i = 0; 100 >= i; i++){
+  console.log(i)
+  if (i % 3 === 0){
+    console.log('dev')
+  } if (i % 5 === 0){
+    console.log("mountain")
+  } if (i % 3 === 0 && i % 5 === 0) {
+    console.log('DevMountian')
+  } if  (i % 3 !== 0 && i % 5 !== 0){
+    console.log(`${i} this number wasn't divis. by 3 or 5`)
+  }
+  }
+
